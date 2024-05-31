4f3e771fdccabc3fad8650ac386c1af8fd204cea
Mahnoor785 <160884071+Mahnoor785@users.noreply.github.com>
2024-05-31 10:27:04 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,40 @@
+import * as inquirer from 'inquirer';
+let todos = [];
+
+inquirer.prompt({
+    name:"To Do List",
+    type:"input",
+    message:"what you want to add in your To Do List ?",
+}
+{}
+name:"addMore",
+type:"confirm",
+message:
+
+}
+]
+}
+
+
+
+
+
+
+
+
+// import inquirer from 'inquirer';
+
+// let todos = [];
+
+// async function addToDo() {
+//     const { task } = await inquirer.prompt({
+//         name: "task",
+//         type: "input",
+//         message: "What do you want to add to your To Do List?",
+//     });
+
+    // todos.push(task);
+//     console.log(`Added "${task}" to your To Do List.`);
+// }
+
+// addToDo();
